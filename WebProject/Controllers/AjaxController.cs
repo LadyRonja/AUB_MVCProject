8034426336966eeaf6a756563b04ac1dac2dacd9
Ronja Mattsson <ronja_mattsson@outlook.com>
2022-05-19 15:42:11 +0100

    ajax database management

---
+++ WebProject/Controllers/AjaxController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nancy.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebProject.Models;
+
+namespace WebProject.Controllers
+{
+    public class AjaxController : Controller
+    {
+        // Use reference to viewmodel as backend/database
+        public static PeopleViewModel database = new PeopleViewModel();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetPeople()
+        {
+            return PartialView("_PersonList", database.AllPeople);
+        }
+
+        public ActionResult GetDetails(string index)
+        {
+            int inputToInt = -1;
+            if (int.TryParse(index, out inputToInt))
+            {
+                if (inputToInt < database.AllPeople.Count)
+                {
+                    return PartialView("_PersonDetails", database.AllPeople[inputToInt]);
+                }
+            }
+
+            return PartialView("_PersonList", database.AllPeople);
+        }
+
+        public ActionResult RemovePerson(string index)
+        {
+            int inputToInt = -1;
+            if (int.TryParse(index, out inputToInt))
+            {
+                if (inputToInt < database.AllPeople.Count)
+                {
+                    database.AllPeople.RemoveAt(inputToInt);
+                }
+            }
+
+            return PartialView("_PersonList", database.AllPeople);
+        }
+
+        /*[HttpPost]
+        public IActionResult jsonexample()
+        {
+            return Json( new { Message = "test" }, new Newtonsoft.Json.JsonSerializerSettings());
+        }*/
+
+        //public IActionResult JSON(string model)
+        //{
+        //    JavaScriptSerializer serializer = new JavaScriptSerializer();
+        //    var fromJSON = serializer.Deserialize<JSONModel>(model);
+        //    return Json(fromJSON);
+        //}
+    }
+}
